c7d0069fd97f10393f01ab3b54d8a55815098073
Rafael 'Monoman' Teixeira <monoman@gmail.com>
2010-05-26 14:43:14 -0300

    unit test project

---
+++ Commons.WebM/Tests.Commons.Ebml/TestElementId.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+using Commons.Ebml;
+
+namespace Tests.Commons.Ebml
+{
+    [TestFixture(Description="Test the utf-8 like serialization of Ids")]
+    public class TestElementId
+    {
+        [Test]
+        public void TestCreationFromBytes() {
+            ElementId id = new ElementId(1, 2, 3, 4);
+            Assert.NotNull(id);
+            Assert.AreEqual(4, id.Bytes.Length);
+            Assert.IsTrue(compareBytes(id.Bytes, new byte[] { 1, 2, 3, 4 }));
+        }
+
+        private bool compareBytes(byte[] first, byte[] second)
+        {
+            if (first == null || second == null || first.Length != second.Length) return false;
+            for (int i = 0; i < first.Length; i++)
+                if (first[i] != second[i])
+                    return false;
+            return true;
+        }
+    }
+}
